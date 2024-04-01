67094b265c9f38dce25dad40eb0620ba8bc7f12c
jennifer <jennifer.omena@outlook.com>
2024-04-01 00:34:26 -0300

    Adicionando um verificador de token

--- bairro-bazar/src/App.js
+++ bairro-bazar/src/App.js
@@ -1,21 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import AppRoutes from './Routes/AppRoutes';
-import Footer from './Components/Footer/Footer';
-
-import './Styles/main.module.scss'
+import React, { useEffect, useState } from 'react'
+import AppRoutes from './Routes/AppRoutes'
 import { UserProvider } from './Context/UserContext'
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
+import api from './Services/api'
+import './Styles/main.module.scss'
 
 function App() {
+  const [userData, setUserData] = useState({
+    isLogged: false,
+    email: '',
+    firstName: '',
+    lastName: '',
+    neighborhood: '',
+    token: ''
+  })
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      api.post('/verify-token', { token })
+        .then(response => {
+          const { firstName, lastName, neighborhood, email } = response.data.user;
+          setUserData({
+            isLogged: true,
+            email,
+            firstName,
+            lastName,
+            neighborhood,
+            token
+          })
+        })
+        .catch(error => {
+          localStorage.removeItem('token');
+          setUserData({
+            isLogged: false,
+            email: '',
+            firstName: '',
+            lastName: '',
+            neighborhood: '',
+            token: ''
+          })
+        })
+    }
+  }, [])
+
   return (
     <>
-      <UserProvider>
+      <UserProvider value={[userData, setUserData]}>
         <AppRoutes />
         <Outlet />
       </UserProvider>
     </>
-    
-  );
+  )
 }
 
-export default App;
+export default App

e14ecae1e01849fd61c4256e4a48bbe2e2966390
jennifer <jennifer.omena@outlook.com>
2024-04-01 00:38:23 -0300

    Adicionando tooltips para informar que em breve haverá esta funcionalidade

--- bairro-bazar/src/Components/Footer/Footer.js
+++ bairro-bazar/src/Components/Footer/Footer.js
@@ -1,23 +1,50 @@
 import React from "react";
-
-import styles from '../../Styles/main.module.scss'
-
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import Logo from '../../assets/logo-bairro-bazar.png'
 import FacebookIcon from "../../assets/facebook-icon.png"
 import InstagramIcon from "../../assets/instagram-icon.png"
 import LinkedinIcon from "../../assets/linkedIn-icon.png"
 import YoutubeIcon from "../../assets/youtube-icon.png"
+import styles from '../../Styles/main.module.scss'
 
 function Footer() {
+    const tooltipText = "Em breve";
+    const tooltipTextSocial = "@bairrobazar"
+
     return (
         <footer className={styles.footer}>
             <div className={styles.footerContainer}>
                 <div className={styles.footerPages}>
-                    <p>Ajuda</p>
-                    <p>Dicas de segurança</p>
-                    <p>Termos de uso</p>
-                    <p>Políticas de privacidade</p>
-                    <p>Propriedade intelectual</p>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip>{tooltipText}</Tooltip>}
+                    >
+                        <p>Ajuda</p>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip>{tooltipText}</Tooltip>}
+                    >
+                        <p>Dicas de segurança</p>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip>{tooltipText}</Tooltip>}
+                    >
+                        <p>Termos de uso</p>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip>{tooltipText}</Tooltip>}
+                    >
+                        <p>Políticas de privacidade</p>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip>{tooltipText}</Tooltip>}
+                    >
+                        <p>Propriedade intelectual</p>
+                    </OverlayTrigger>
                 </div>
                 <div className={styles.footerCopy}>
                     <img style={{height: "60%"}} src={Logo} alt="Logo Bairro Bazar"/>
@@ -25,10 +52,32 @@ function Footer() {
                         <p>Bairro Bazar © 2024 - todos os direitos reservados - Bairro Bazar LTDA. CNPJ: 00.000.000/000-00</p>
                     </div>
                     <div className={styles.footerSocialMedias}>
-                        <img style={{height: "60%"}} src={FacebookIcon} alt="Facebook"/>
-                        <img style={{height: "60%"}} src={InstagramIcon} alt="Instagram"/>
-                        <img style={{height: "60%"}} src={LinkedinIcon} alt="Linkedin"/>
-                        <img style={{height: "60%"}} src={YoutubeIcon} alt="Youtube"/>
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={<Tooltip>{tooltipTextSocial}</Tooltip>}
+                        >
+                            <img style={{height: "60%"}} src={FacebookIcon} alt="Facebook"/>
+                        </OverlayTrigger>
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={<Tooltip>{tooltipTextSocial}</Tooltip>}
+                        >
+                            <img style={{height: "60%"}} src={InstagramIcon} alt="Instagram"/>
+                        </OverlayTrigger>
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={<Tooltip>{tooltipTextSocial}</Tooltip>}
+                        >
+                            <img style={{height: "60%"}} src={LinkedinIcon} alt="Linkedin"/>
+                        </OverlayTrigger>
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={<Tooltip>{tooltipTextSocial}</Tooltip>}
+                        >
+                            <img style={{height: "60%"}} src={YoutubeIcon} alt="Youtube"/>
+                        </OverlayTrigger>
+                        
+                        
                     </div>
                 </div>
             </div>
--- bairro-bazar/src/Components/MenuCard/MenuCard.js
+++ bairro-bazar/src/Components/MenuCard/MenuCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import ArtigosInfantis from '../../assets/artigos-infantis.png'
 import AutosPecas from '../../assets/autos-pecas.png'
 import CasaDecoracao from '../../assets/casa-decoracao.png'
@@ -8,48 +8,95 @@ import EsporteLazer from '../../assets/esporte-lazer.png'
 import ModaBeleza from '../../assets/moda-beleza.png'
 import MusicasHobbies from '../../assets/musicas-hobbies.png'
 import ServicosVagas from '../../assets/servicos-vagas.png'
-
 import styles from '../../Styles/main.module.scss'
 
 function MenuCard() {
+    const tooltipText = "Em breve";
+
     return (
         <section className={styles.inputSection}>
             <div className={styles.menuCard}>
-                <div className={styles.cards}>
-                    <img src={ArtigosInfantis} alt=""/>
-                    <p className={styles.menuCardText}>Artigos Infantis</p>
-                </div>
-                <div className={styles.cards}>
-                    <img src={AutosPecas} alt=""/>
-                    <p className={styles.menuCardText}>Autos e Peças</p>
-                </div>
-                <div className={styles.cards}>
-                    <img src={CasaDecoracao} alt=""/>
-                    <p className={styles.menuCardText}>Casa e Decoração</p>
-                </div>
-                <div className={styles.cards}>
-                    <img src={EletronicosCelulares} alt=""/>
-                    <p className={styles.menuCardText}>Eletrônicos e Celulares</p>
-                </div>
-                <div className={styles.cards}>
-                    <img src={EsporteLazer} alt=""/>
-                    <p className={styles.menuCardText}>Esporte e Lazer</p>
-                </div>
-                <div className={styles.cards}>
-                    <img src={ModaBeleza} alt=""/>
-                    <p className={styles.menuCardText}>Moda e Beleza</p>
-                </div>
-                <div className={styles.cards}>
-                    <img src={MusicasHobbies} alt=""/>
-                    <p className={styles.menuCardText}>Músicas e Hobbies</p>
-                </div>
-                <div className={styles.cards}>
-                    <img src={ServicosVagas} alt=""/>
-                    <p className={styles.menuCardText}>Serviços e Vagas</p>
-                </div>
+                <OverlayTrigger
+                    placement="bottom"
+                    overlay={<Tooltip>{tooltipText}</Tooltip>}
+                >
+                    <div className={styles.cards}>
+                        <img src={ArtigosInfantis} alt="artigos-infantis"/>
+                        <p className={styles.menuCardText}>Artigos Infantis</p>
+                    </div>
+                </OverlayTrigger>
+                
+                <OverlayTrigger
+                    placement="bottom"
+                    overlay={<Tooltip>{tooltipText}</Tooltip>}
+                >
+                    <div className={styles.cards}>
+                        <img src={AutosPecas} alt=""/>
+                        <p className={styles.menuCardText}>Autos e Peças</p>
+                    </div>
+                </OverlayTrigger>
+
+                <OverlayTrigger
+                    placement="bottom"
+                    overlay={<Tooltip>{tooltipText}</Tooltip>}
+                >
+                    <div className={styles.cards}>
+                        <img src={CasaDecoracao} alt=""/>
+                        <p className={styles.menuCardText}>Casa e Decoração</p>
+                    </div>
+                </OverlayTrigger>
+
+                <OverlayTrigger
+                    placement="bottom"
+                    overlay={<Tooltip>{tooltipText}</Tooltip>}
+                >
+                    <div className={styles.cards}>
+                        <img src={EletronicosCelulares} alt=""/>
+                        <p className={styles.menuCardText}>Eletrônicos e Celulares</p>
+                    </div>
+                </OverlayTrigger>
+
+                <OverlayTrigger
+                    placement="bottom"
+                    overlay={<Tooltip>{tooltipText}</Tooltip>}
+                >
+                    <div className={styles.cards}>
+                        <img src={EsporteLazer} alt=""/>
+                        <p className={styles.menuCardText}>Esporte e Lazer</p>
+                    </div>
+                </OverlayTrigger>
+
+                <OverlayTrigger
+                    placement="bottom"
+                    overlay={<Tooltip>{tooltipText}</Tooltip>}
+                >
+                    <div className={styles.cards}>
+                        <img src={ModaBeleza} alt=""/>
+                        <p className={styles.menuCardText}>Moda e Beleza</p>
+                    </div>
+                </OverlayTrigger>
+
+                <OverlayTrigger
+                    placement="bottom"
+                    overlay={<Tooltip>{tooltipText}</Tooltip>}
+                >
+                    <div className={styles.cards}>
+                        <img src={MusicasHobbies} alt=""/>
+                        <p className={styles.menuCardText}>Músicas e Hobbies</p>
+                    </div>
+                </OverlayTrigger>
+
+                <OverlayTrigger
+                    placement="bottom"
+                    overlay={<Tooltip>{tooltipText}</Tooltip>}
+                >
+                    <div className={styles.cards}>
+                        <img src={ServicosVagas} alt=""/>
+                        <p className={styles.menuCardText}>Serviços e Vagas</p>
+                    </div>
+                </OverlayTrigger>
             </div>
         </section>
-        
     )
 }
 
